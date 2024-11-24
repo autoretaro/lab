f2e956f1e4239214022c65e88951ec1ad9564c6f
Vanderlei Neto <jv.neto@outlook.com.br>
2024-11-24 19:28:49 -0300

    Ajusndo footer

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -1,12 +1,24 @@
 import "../Footer/Footer.css";
 import logoPatrocinioOrizonal from "../../assets/img/main/logo-orizontal.png";
-import logoPatrocinioVertical from "../../assets/img/main/logo-vertical.png"
+import logoPatrocinioVertical from "../../assets/img/main/logo-vertical.png";
+import logoRedeEparrei from "../../assets/img/main/Rede_eparrei.png";
+import logoEparreiFilmes from "../../assets/img/main/Eparrei_filmes.png";
+import logoKatende from "../../assets/img/main/Logo_katende.png";
 
 
-// eslint-disable-next-line react/prop-types, no-unused-vars
-const CreateFooter = ({ paragraph, imgSrc }) => {
+const CreateLogoApoio = ({imgSrc }) => {
+  return (
+    <div className="container__footer">
+      <img src={imgSrc} alt="" className="" />
+      <div className="">
+      </div>
+    </div>
+    );
+}
+
+const CreateLogoPatrocinio = ({imgSrc }) => {
     return (
-      <footer>
+      <div>
       <div className="container__footer">
         <img src={imgSrc} alt="" className="" />
         <div className="">
@@ -17,19 +29,32 @@ const CreateFooter = ({ paragraph, imgSrc }) => {
         <p >Esse projeto foi fomentado <strong>Programa Funarte Retomada 2023 - Artes Visuais</strong></p>
         <a  className="paragraph" href="https://vanderleineto.tech/" target="_blank">Feito por Vanderlei Neto</a>
         </div>
-      </footer>
+      </div>
     );
   };
+  
+  
 
 const Footer = () => {
     return(
         <div className="container__footer">
+          <div className="footer__rede-parrei">
+            <CreateLogoApoio imgSrc={logoRedeEparrei} />
+          </div>
+          <div className="footer__rede-parrei">
+            <CreateLogoApoio imgSrc={logoEparreiFilmes} />
+          </div>
+          <div className="footer_katende_filmes">
+            <CreateLogoApoio imgSrc={logoKatende} />
+          </div>
             <div className="footer__orizontal">
-            <CreateFooter imgSrc={logoPatrocinioOrizonal} />
+            <CreateLogoPatrocinio imgSrc={logoPatrocinioOrizonal} />
+            
             </div>
             <div className="footer__vertical">
-            <CreateFooter imgSrc={logoPatrocinioVertical}/>
+            <CreateLogoPatrocinio imgSrc={logoPatrocinioVertical}/>
             </div>
+            
         </div>
     )
 }
