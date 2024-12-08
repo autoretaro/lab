db5e2a83e57adbb74373167de2d8fefbcd0c5e4c
Vanderlei Neto <jv.neto@outlook.com.br>
2024-12-08 15:50:47 -0300

    Deploy e geristro de dominio

--- src/Componets/Header/Menu.jsx
+++ src/Componets/Header/Menu.jsx
@@ -2,45 +2,71 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import "../Header/Menu.css";
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "../../assets/img/main/Autoretrato.svg";
 
-//oooo
 function Menu() {
   return (
     <header>
-    <Navbar expand="lg" className="bg">
-      <Container>
-        <Navbar.Brand as={Link} to="/"><img src={logo} alt="" className='logo'/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto fs-5"> {/* Use ms-auto to align items to the right */}
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">Sobre</Nav.Link>
-            <Nav.Link as={Link} to="/catalog">Catálogo</Nav.Link>
-            <Nav.Link as={Link} to="/poetry">Poesia</Nav.Link>
-            <NavDropdown title="Redes Socias" id="basic-nav-dropdown">
-              {/* <NavDropdown.Item as={Link} to="/action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/action/3.2">
-                Another action
-              </NavDropdown.Item> */}
-              <NavDropdown.Item as={Link} to="/action/3.3"></NavDropdown.Item>
-              <NavDropdown.Divider />
-                  <NavDropdown.Item href="https://www.instagram.com" target="_blank" className='fs-5'>
-              <i className="fab fa-instagram"></i> Instagram
-            </NavDropdown.Item>
-            <NavDropdown.Item href="https://www.facebook.com" target="_blank" className='fs-5'>
-              <i className="fab fa-facebook"></i> Facebook
-            </NavDropdown.Item>
-
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>
+      <Navbar expand="lg" className="bg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="" className="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto fs-5">
+              {/* Adicione classes do Bootstrap */}
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                className="text-dark fw-bold hover-scale"
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link 
+                as={Link} 
+                to="/about" 
+                className="text-dark fw-bold hover-scale"
+              >
+                Sobre
+              </Nav.Link>
+              <Nav.Link 
+                as={Link} 
+                to="/catalog" 
+                className="text-dark fw-bold hover-scale"
+              >
+                Catálogo
+              </Nav.Link>
+              <Nav.Link 
+                as={Link} 
+                to="/poetry" 
+                className="text-dark fw-bold hover-scale"
+              >
+                Poesia
+              </Nav.Link>
+              <NavDropdown title="Redes Sociais" id="basic-nav-dropdown" className="text-dark">
+                <NavDropdown.Item
+                  href="https://www.instagram.com"
+                  target="_blank"
+                  className="fs-5 text-dark fw-bold hover-scale"
+                >
+                  <i className="fab fa-instagram"></i> Instagram
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  className="fs-5 text-dark fw-bold hover-scale"
+                >
+                  <i className="fab fa-facebook"></i> Facebook
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
